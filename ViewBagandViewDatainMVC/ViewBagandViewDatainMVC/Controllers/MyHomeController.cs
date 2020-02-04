6d005d059022fac8a73346a4265acde96d13e8fb
HemantPundpal <47489403+HemantPundpal@users.noreply.github.com>
2020-02-04 15:12:47 +1300

    Introduction to MVC - ViewBag and ViewData

---
+++ ViewBagandViewDatainMVC/ViewBagandViewDatainMVC/Controllers/MyHomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewBagandViewDatainMVC.Controllers
+{
+    public class MyHomeController : Controller
+    {
+        // GET: MyHome
+        public ActionResult IndexViewBag()
+        {
+            /* Note: ViewBag uses Dictionary name value internaly to store the data. */
+            ViewBag.Countries = new List<string>()
+            {
+                "New Zealand",
+                "India",
+                "USA",
+                "UK",
+                "Canada"
+            };
+            return View();
+        }
+
+        public ActionResult IndexViewData()
+        {
+            ViewData["COUNTRIES"] = new List<string>()
+            {
+                "New Zealand",
+                "India",
+                "USA",
+                "UK",
+                "Canada"
+            };
+            return View();
+        }
+    }
+}
---
+++ ViewBagandViewDatainMVC/ViewBagandViewDatainMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ViewBagandViewDatainMVC.MvcApplication" Language="C#" %>
