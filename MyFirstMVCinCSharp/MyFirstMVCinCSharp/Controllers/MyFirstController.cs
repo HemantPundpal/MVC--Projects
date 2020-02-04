78187526478f81dee8e5993cb6cfbba9cd958222
HemantPundpal <47489403+HemantPundpal@users.noreply.github.com>
2020-02-04 15:07:59 +1300

    Introduction to MVC - Controller

---
+++ MyFirstMVCinCSharp/MyFirstMVCinCSharp/Controllers/MyFirstController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCinCSharp.Controllers
+{
+    public class MyFirstController : Controller
+    {
+        // GET: MyFirst
+        public /*ActionResult*/ string Index(string Id, string name)
+        {
+            //return View();
+            //return $"Hello from my first MVC controller {Environment.NewLine}Id = {Id} Name = {Request.QueryString["Name"]}";
+            return $"Hello from my first MVC controller {Environment.NewLine}Id = {Id} Name = {name}";
+        }
+
+        public ActionResult IndexView() //Action result is commented as it is bundled with other Views
+        {
+            ViewBag.Countries = new List<string>()
+            {
+                "New Zealand",
+                "India",
+                "USA",
+                "UK",
+                "Canada"
+            };
+
+            return View();
+        }
+    }
+}
---
+++ MyFirstMVCinCSharp/MyFirstMVCinCSharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFirstMVCinCSharp.MvcApplication" Language="C#" %>
